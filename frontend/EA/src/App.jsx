1a84136ec4b2ba0ab991e4d0e0d059537ac5f8e3
Riteshgarlapati <riteshgarlapati20@gmail.com>
2024-08-29 19:58:30 +0530

    routes and signup API req

--- frontend/EA/src/App.jsx
+++ frontend/EA/src/App.jsx
@@ -1,16 +1,19 @@
-// import { useState } from 'react'
-import './App.css'
-import Login from './components/Login'
-import Register from "./components/Register"
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Signup from "./components/Register"; // Assuming SignupForm is the Register component
+import Login from "./components/Login"; // Create or import your Login component
 
-function App() {
-
+const App = () => {
   return (
-    <div>
-      <Register/>
-      <Login/>
-    </div>
-  )
-}
+    <Router>
+      <div className="min-h-screen bg-blue-100 flex items-center justify-center">
+        <Routes>
+          <Route path="/register" element={<Signup />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
