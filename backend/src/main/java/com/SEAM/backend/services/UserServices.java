2ee39cb7ddcac00da67634ef6c6ab110ddede590
Akhilkumar6 <akhil06122002@gmail.com>
2024-08-31 14:04:42 +0530

    login

--- backend/src/main/java/com/SEAM/backend/services/UserServices.java
+++ backend/src/main/java/com/SEAM/backend/services/UserServices.java
@@ -1,5 +1,6 @@
 package com.SEAM.backend.services;
 
+import com.SEAM.backend.models.UserBody;
 import com.SEAM.backend.models.UserModel;
 import com.SEAM.backend.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 
@@ -15,14 +17,23 @@ public class UserServices {
     @Autowired
     public UserRepo repo;
 
-    public UserModel putData(UserModel user){
+    public UserModel putData(UserModel user) {
 
         return repo.save(user);
 
     }
 
-    public List<UserModel> search(String email){
-        return repo.findByEmail(email);
+    public String searchUser(UserBody user) {
+        try {
+            UserModel u = repo.findByEmail(user.email).getFirst();
+            if (Objects.equals(u.password, user.password)){
+                return "Auth";
+            }
+            else
+                return "AuthFuck";
+        } catch (Exception e) {
+            return "NoUser";
+        }
     }
 
     }
