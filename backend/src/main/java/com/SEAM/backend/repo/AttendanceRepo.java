1df1bd8447f09cbbc3ebfd13819913f22d68f74b
snuhith95 <snuhithbobbala@gmail.com>
2024-09-03 20:19:44 +0530

    xl done

--- backend/src/main/java/com/SEAM/backend/repo/AttendanceRepo.java
+++
@@ -1,8 +0,0 @@
-package com.SEAM.backend.repo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.SEAM.backend.models.Attendance;
-
-public interface AttendanceRepo extends MongoRepository<Attendance,String>{
-    
-}
